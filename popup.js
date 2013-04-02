c0dd7c30530ee3fc8cd9c46ee7e039cf342d1a38
dingzj <dingzejin@gmail.com>
2013-04-02 12:55:41 -0400

    allow to retrive several checked settings, and selecting settings together

--- popup.js
+++ popup.js
@@ -1,33 +1,80 @@
-function getGlobalSettings()
-{
+
+function getRadioSetting(url, findSettingID, setDivID, setInputID, setLabel) {
 	var xmlhttp = new XMLHttpRequest();
-	var url = "https://www.linkedin.com/settings/activity-broadcasts";
-	//var csrfToken = document.getElementById("nav-utility-auth").childNodes[0].href.split(/[=&]/)[3];
 	var csrfToken = document.getElementById("csrfToken").value;
 	var params = "csrfToken="+csrfToken;
 	xmlhttp.open("GET", url, true);
 	xmlhttp.withCredentials = true;
 	xmlhttp.setRequestHeader("content-type", "application/x-www-form-urlencoded");
 	xmlhttp.send(params);
+	
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState == 4) {
-			var response = xmlhttp.responseText;
-			var xmlDoc = $(response);
-			var msgElem  = xmlDoc.find("#activity-activity-editActivityBroadcasts");
+			var xmlDoc = $(xmlhttp.responseText);
+			var msgElem  = xmlDoc.find("#"+findSettingID);
 			if (msgElem[0].checked === true) {
-				$("#insert-activity-broadcasts").html("<input type=\"checkbox\" name=\"activity-broadcasts\" value=\"activity\" checked> <label for=\"activity-broadcasts\">activity broadcasts</label> ");
+				$("#"+setDivID).html("<input type=\"checkbox\" id=\"" + setInputID + "\" value=\"set\" checked> <label for=\"" + setInputID + "\"> " + setLabel + "</label> ");
 			} else {
-				$("#insert-activity-broadcasts").html("<input type=\"checkbox\" name=\"activity-broadcasts\" value=\"activity\" > <label for=\"activity-broadcasts\">activity broadcasts</label>");
+				$("#"+setDivID).html("<input type=\"checkbox\" id=\"" + setInputID + "\" value=\"set\"> <label for=\"" + setInputID + "\"> " + setLabel + "</label> ");
 			}
 		}
-	};
+	}
 }
+
+function getOptionSetting(url, findSettingID, setDivID, setInputID, setLabel) {
+	var xmlhttp = new XMLHttpRequest();
+	var csrfToken = document.getElementById("csrfToken").value;
+	var params = "csrfToken="+csrfToken;
+	xmlhttp.open("GET", url, true);
+	xmlhttp.withCredentials = true;
+	xmlhttp.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+	xmlhttp.send(params);
+	
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4) {
+			var xmlDoc = $(xmlhttp.responseText);
+			var nodes  = xmlDoc.find("#"+findSettingID)[0].childNodes;
+			var html = "<label for=\"" + setDivID + "\"> " + setLabel + "</label> <br />";
+			for (i=1; i<nodes.length; i++) {
+				var checked = "";
+				if (nodes[i].getAttribute("selected") == "") {
+					checked = " checked ";
+				}
+				html += "<input type=\"radio\" id=\"" + setInputID + "\" value=\"" + nodes[i].value + "\"  name=\"" + setInputID + "\" " + checked + " /> <label for=\"" + setInputID + "\"> " + nodes[i].text + "</label> <br /> \n" ;
+			}
+			$("#"+setDivID).html(html);
+		}
+	}
+}
+
+function getAllSettings() {
+	var url = "https://www.linkedin.com/settings/activity-broadcasts";
+	var findID = "activity-activity-editActivityBroadcasts";
+	var setDivID = "div-activity-broadcasts";
+	var setInputID = "input-activity-broadcasts";
+	var setLabel = "Show acvitity broadcasting";
+	getRadioSetting(url, findID, setDivID, setInputID, setLabel);
+	
+	var url2 = "https://www.linkedin.com/settings/browse-map";
+	var findID2 = "browseMapParam-browseMapParam-browseMap";
+	var setDivID2 = "div-browseparam-browsemap";
+	var setInputID2 = "input-browseparam-browsemap";
+	var setLabel2 = "Show people viewed your profile also viewed";
+	getRadioSetting(url2, findID2, setDivID2, setInputID2, setLabel2);
+	
+	var url3 = "https://www.linkedin.com/settings/activity-visibility";
+	var findID3 = "activityFeed-editActivityFeed";
+	var setDivID3 = "div-activityfeed-activityfeed";
+	var setSelectID3 = "select-activityfeed-activityfeed";
+	var setLabel3 = "Who can see your activity feed: ";
+	getOptionSetting(url3, findID3, setDivID3, setSelectID3, setLabel3);
+}
+
 
 function setGlobalSettings()
 {
   var value = $('input[@name="global-level"]:checked').val();
 	var xmlhttp = new XMLHttpRequest();
-	//xmlhttp.open("GET", "https://www.linkedin.com/settings/activity-broadcasts?goback=%2Enas_*1_*1_*1", true);
 	var url = "https://www.linkedin.com/settings/activity-broadcasts-submit";
 	var csrfToken = document.getElementById("csrfToken").value;
 	var activity = (value == "High" ? "activity" : "");
@@ -49,15 +96,15 @@ function setGlobalSettings()
 }
 
 function onPageInfo(o) { 
-	document.getElementById("title").value = o.title; 
-	document.getElementById("url").value = o.url; 
+	//document.getElementById("title").value = o.title; 
+	//document.getElementById("url").value = o.url; 
 	document.getElementById("csrfToken").value = o.csrfToken; 
 } 
 
 function test() {
 	console.log("11111111 found xxxxxxxxxx ");
-	$("#btn-set-global-level").click(setGlobalSettings);
-	$("#btn-get-activity-broadcasts").click(getGlobalSettings);
+	$("#btn-set-all-settings").click(setGlobalSettings);
+	$("#btn-get-all-settings").click(getAllSettings);
   var bg = chrome.extension.getBackgroundPage();
   bg.getPageInfo(onPageInfo);
 }
