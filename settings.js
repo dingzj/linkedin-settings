8a6af856e6b55e4e791ec20695ac41e35d5169b4
dingzj <dingzejin@gmail.com>
2013-04-05 14:33:41 -0400

    move variables to settings.js

---
+++ settings.js
@@ -0,0 +1,66 @@
+var ab = {
+	"name": "activity-broadcasts",
+	"findID":	"activity-activity-editActivityBroadcasts",
+	"setLabel": "Show acvitity broadcasting",
+	"setVarName": "activity",
+};
+
+var bm = {
+	"name": "browse-map",
+	"findID":	"browseMapParam-browseMapParam-browseMap",
+	"setLabel": "Show people viewed your profile also viewed",
+	"setVarName": "browseMapParam"
+};
+
+var cv = {
+	"name": "connection-visibility",
+	"findID":	"browseConnections-editBrowseConnections",
+	"setLabel": "Who can see your connection: ",
+	"setVarName": "browseConnections"
+};
+
+var av = {
+	"name": "activity-visibility",
+	"findID":	"activityFeed-editActivityFeed",
+	"setLabel": "Who can see your activity feed: ",
+	"setVarName": "activityFeed"
+};
+
+var la = {
+	"name": "li-announcements",
+	"findID":	"liAnnouncementsParam-liAnnouncementsParam-editReceivingMarketing",
+	"setLabel": "Get Linkedin announcement ",
+	"setVarName": "liAnnouncementsParam"
+};
+
+var ri = {
+	"name": "research-invitations",
+	"findID":	"researchInvitationsParam-researchInvitationsParam-editResearchInvitations",
+	"setLabel": "Invitations to participate in research",
+	"setVarName": "researchInvitationsParam"
+};
+
+var pim = {
+	"name": "partner-inmail",
+	"submitPath": "https://www.linkedin.com/settings/partner-inMail",
+	"setName": "partner-inmail-marketing",
+	"findID":	"marketingPartnerParam-marketingPartnerParam-editPartnerInMail",
+	"setLabel": "Linkedin's Marketing partners may send you information",
+	"setVarName": "marketingPartnerParam"
+};
+
+var pih = {
+	"name": "partner-inmail",
+	"submitPath": "https://www.linkedin.com/settings/partner-inMail",
+	"setName": "partner-inmail-hiring",
+	"findID":	"hiringCampaignParam-hiringCampaignParam-editPartnerInMail",
+	"setLabel": "Linkedin's hiring campaign may send you information",
+	"setVarName": "hiringCampaignParam"
+};
+
+var ppv = {
+	"name": "profile-photo-visibility",
+	"findID":	"profilePhotosParam-editProfilePhotos",
+	"setLabel": "Select whose photos you would like to see",
+	"setVarName": "profilePhotosParam"	
+};
